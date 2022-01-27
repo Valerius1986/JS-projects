19ae4e248f6232eaacd6a1c0d47c8ecb53e31eb9
Valeriy <valcom1986@gmail.com>
2022-01-27 11:34:55 +0300

    Task 2 - Completed

--- js/script.js
+++ js/script.js
@@ -25,5 +25,11 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
